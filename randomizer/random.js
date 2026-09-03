50af5f698128f41877f7f58ef9655c7498ff5637
main <kirilmashlakov@gmail.com>
2026-09-03 13:40:13 +0500

    Initial commit

---
+++ randomizer/random.js
@@ -0,0 +1,10 @@
+const O_R = document.querySelector("#O_R");
+const manyVar = document.querySelector("#manyVar");
+const vip = document.querySelector("#vip");
+
+const cap = document.querySelector("#cap");
+
+O_R.addEventListener('click', function() {
+    return(O_R.text.content = "ОРЕЛ-РЕШКА")
+})
+
