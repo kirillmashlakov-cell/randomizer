b33d80497cc84e98fd009997a35cfbb9ba6e12ca
main <kirilmashlakov@gmail.com>
2026-09-04 12:40:22 +0500

    Implement coin flip game

--- randomizer/random.js
+++ randomizer/random.js
@@ -1,10 +1,42 @@
-const O_R = document.querySelector("#O_R");
-const manyVar = document.querySelector("#manyVar");
-const vip = document.querySelector("#vip");
+const O_R = document.getElementById('O_R');
+const manyVar = document.getElementById('manyVar');
+const vip = document.getElementById('vip');
 
-const cap = document.querySelector("#cap");
+const cap = document.getElementById('cap');
+
+const button_O_R = document.querySelector("#button_O_R");
+const rezult = document.querySelector("#rezult");
+const orel = document.querySelector("#orel");
+const reshka = document.querySelector("#reshka");
+
 
 O_R.addEventListener('click', function() {
-    return(O_R.text.content = "ОРЕЛ-РЕШКА")
-})
+    cap.textContent = "ОРЕЛ-РЕШКА";
+});
 
+
+manyVar.addEventListener('click', function() {
+    cap.textContent = "Несколько вариантов";
+});
+
+
+vip.addEventListener('click', function() {
+    cap.textContent = "ну это ты хорошо придумал";
+});
+
+
+let count_orel = 0;
+let count_reshka = 0; 
+button_O_R.addEventListener('click', function () {
+    let rez = Math.floor(Math.random() * 2);
+
+      if (rez === 0) {
+      count_orel  +=  1;
+        rezult.textContent = "Орел";
+        orel.textContent = count_orel;
+      } else{
+        count_reshka  += 1;
+        rezult.textContent = "Решка";
+        reshka.textContent = count_reshka;
+         }
+})
